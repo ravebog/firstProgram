6ee24ac896c2cc4dfb247eee2a973266777f60e8
Bianca <58823326+cribibi@users.noreply.github.com>
2020-10-28 17:44:58 +0200

    Update Scrabble.java
    another comment

--- src/main/java/com/sda/exercitii/Scrabble.java
+++ src/main/java/com/sda/exercitii/Scrabble.java
@@ -3,6 +3,7 @@ package com.sda.exercitii;
 import java.util.*;
 
 public class Scrabble {
+    //comment for testing git
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please insert a number of words");

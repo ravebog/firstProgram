8efc8c01aa62a0bdba1d19ba825e49f09d0033e7
ravebog <73530923+ravebog@users.noreply.github.com>
2020-10-28 18:10:48 +0200

    Update Scrabble.java

--- src/main/java/com/sda/exercitii/Scrabble.java
+++ src/main/java/com/sda/exercitii/Scrabble.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Scrabble {
     //comment for testing git
+    //alt comment
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Please insert a number of words");
